1c47df9fb26e8fbc635e721680896aaec3059bd6
johannesnap <johannes_h_napitupulu@telkomsel.co.id>
2018-09-29 15:26:21 +0700

    First commit

---
+++ albums/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const Header = (props) => {
+        return (
+            <View>
+                <Text>
+                    {props.headerText}
+                </Text>
+            </View>
+        )
+};
+
+
+export default Header
