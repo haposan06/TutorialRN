e00fb38e60ed5efc17b7a70a51fcb6fa4355c1b3
unknown <Johannes@PBT218038.telkomsel.co.id>
2018-10-01 01:13:11 +0700

    Second commit

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,35 @@
 import React, {Component} from 'react'
 import {View, Text} from 'react-native'
+import firebase from 'firebase'
 import {Header} from "./components/common";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
+    state = {loggedIn: false}
+    componentWillMount(){
+        var config = {
+            apiKey: "AIzaSyB43HhA7h8Oig5R2cknBGAkTacqBhnjOO0",
+            authDomain: "authentication-fe6cf.firebaseapp.com",
+            databaseURL: "https://authentication-fe6cf.firebaseio.com",
+            projectId: "authentication-fe6cf",
+            storageBucket: "authentication-fe6cf.appspot.com",
+            messagingSenderId: "864353311782"
+        };
+        firebase.initializeApp(config);
+
+        firebase.auth().onAuthStateChanged((user)=>{
+            if (user){
+                this.setState({loggedIn: true});
+            } false {
+                this.setState({loggedIn: false});
+            }
+        });
+    }
     render(){
         return (
             <View>
                 <Header headerText="Authentication"/>
-                <Text>Inside app</Text>
+                <LoginForm/>
             </View>
         )
     };
---
+++ auth/src/components/LoginForm.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react'
+import firebase from 'firebase'
+import {Text} from 'react-native'
+import {Button, Card, CardSection, Input, Spinner} from "./common";
+
+class LoginForm extends Component {
+
+    state = {email: '', password: '', error: '', loading: false};
+
+    renderButton(){
+        if (this.state.loading){
+            return <Spinner size="small"/>
+        }
+
+        return(
+            <Button
+                onPress={this.onButtonPress.bind(this)}
+            >
+                Log In!
+            </Button>
+        )
+    }
+
+    onLoginSuccess(msg){
+        this.setState({
+            email: '',
+            password: '',
+            loading: false,
+            error : msg
+        });
+    }
+
+    onLoginFailed(){
+        this.setState({
+            error: 'Authentication Failed',
+            loading: false
+        });
+    }
+    onButtonPress(){
+        const {email, password} = this.state;
+        this.setState({error: '', loading: true});
+        firebase.auth().signInWithEmailAndPassword(email,password)
+            .then(this.onLoginSuccess.bind(this))
+            .catch(()=>{
+                firebase.auth(). createUserWithEmailAndPassword(email, password)
+                    .then(this.onLoginSuccess.bind(this))
+                    .catch(this.onLoginFailed.bind(this));
+                })
+            });
+    }
+
+    render(){
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        placeholder="Input your email here"
+                        label="Email"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({email})}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        placeholder="password"
+                        value={this.state.password}
+                        label="Password"
+                        onChangeText={password => this.setState({password})}
+                    />
+                </CardSection>
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+                <CardSection>
+                    {this.renderButton()}
+                </CardSection>
+            </Card>
+        )
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
+
+export default LoginForm;
